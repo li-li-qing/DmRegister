fae3705eee237733785fd1902b650c2801ce940d
li-li-qing <975223642@qq.com>
2025-04-15 16:25:55 +0800

    换了一种写法

--- DMHelper.cpp
+++ DMHelper.cpp
@@ -1,69 +1,71 @@
 ﻿// 文件: DMHelper.cpp
-// 简介: 实现大漠插件的线程安全封装
-// 详细: 实现通过免注册方式动态加载 dm.dll 并获取 Idmsoft 接口对象，提供线程安全的单例获取及释放方法
+// 简介: 实现大漠插件封装类
+// 详细: 初始化 dm.dll，提供资源路径和字库设置封装
 
 #include "pch.h"
 #include "DMHelper.h"
 
-Idmsoft* CDMHelper::m_pDm = nullptr;
-CRITICAL_SECTION CDMHelper::m_cs;
-bool CDMHelper::m_bInit = false;
-
 // 文件: DMHelper.cpp
-// 简介: 初始化线程同步资源
-// 详细: 初始化 Critical Section，保证后续访问线程安全，仅执行一次
-void CDMHelper::Init()
+// 简介: 构造函数
+// 详细: 加载 dm.dll 并创建 Idmsoft 对象
+CDMHelper::CDMHelper()
 {
-	if (!m_bInit)
-	{
-		InitializeCriticalSection(&m_cs);
-		m_bInit = true;
-	}
-}
+	m_pDm = nullptr;
 
-// 文件: DMHelper.cpp
-// 简介: 获取大漠插件单例对象
-// 详细: 动态加载 dm.dll，获取 DllGetClassObject 函数，通过 COM 接口创建 Idmsoft 实例，线程安全
-Idmsoft* CDMHelper::GetInstance()
-{
-	Init();
-	EnterCriticalSection(&m_cs);
-
-	if (m_pDm == nullptr)
+	typedef HRESULT(_stdcall* pfnGCO)(REFCLSID, REFIID, void**);
+	pfnGCO fnGCO = nullptr;
+	HINSTANCE hDll = LoadLibrary(_T("dm.dll"));
+	if (hDll)
 	{
-		typedef HRESULT(_stdcall* pfnGCO)(REFCLSID, REFIID, void**);
-		pfnGCO fnGCO = nullptr;
-		HINSTANCE hDll = LoadLibrary(_T("dm.dll"));
-		if (hDll)
+		fnGCO = (pfnGCO)GetProcAddress(hDll, "DllGetClassObject");
+		if (fnGCO)
 		{
-			fnGCO = (pfnGCO)GetProcAddress(hDll, "DllGetClassObject");
-			if (fnGCO)
+			IClassFactory* pcf = nullptr;
+			if (SUCCEEDED(fnGCO(__uuidof(dmsoft), IID_IClassFactory, (void**)&pcf)) && pcf)
 			{
-				IClassFactory* pcf = nullptr;
-				HRESULT hr = fnGCO(__uuidof(dmsoft), IID_IClassFactory, (void**)&pcf);
-				if (SUCCEEDED(hr) && pcf)
-				{
-					hr = pcf->CreateInstance(nullptr, __uuidof(Idmsoft), (void**)&m_pDm);
-					pcf->Release();
-				}
+				pcf->CreateInstance(nullptr, __uuidof(Idmsoft), (void**)&m_pDm);
+				pcf->Release();
 			}
 		}
 	}
-
-	LeaveCriticalSection(&m_cs);
-	return m_pDm;
 }
 
 // 文件: DMHelper.cpp
-// 简介: 释放大漠插件对象
-// 详细: 清空并释放大漠插件的 COM 实例，适用于程序退出时的清理工作，线程安全
-void CDMHelper::ReleaseInstance()
+// 简介: 析构函数
+// 详细: 释放 Idmsoft 对象，防止资源泄漏
+CDMHelper::~CDMHelper()
 {
-	EnterCriticalSection(&m_cs);
 	if (m_pDm)
 	{
 		m_pDm->Release();
 		m_pDm = nullptr;
 	}
-	LeaveCriticalSection(&m_cs);
 }
+
+// 文件: DMHelper.cpp
+// 简介: 获取大漠插件接口指针
+// 详细: 返回 m_pDm 指针，可用于调用大漠所有原始函数
+Idmsoft* CDMHelper::GetDm()
+{
+	return m_pDm;
+}
+
+// 文件: DMHelper.cpp
+// 简介: 设置资源路径
+// 详细: 设置大漠查图、识字所使用的基础目录，如 .\\Ocr
+bool CDMHelper::SetDmResourcePath(const CString& path)
+{
+	// 设置大漠路径
+	return m_pDm && m_pDm->SetPath((LPCTSTR)path) == 1;
+}
+
+
+// 文件: DMHelper.cpp
+// 简介: 设置大漠插件字库（自动创建 .txt 字库）
+// 详细: 如果指定 .txt 字库文件不存在，则自动创建一个空的文本文件并设置到指定槽位
+bool CDMHelper::SetFontDict(int index, const CString& fontFile)
+{
+	// 设置字库到指定槽位
+	return m_pDm && m_pDm->SetDict(index, (LPCTSTR)fontFile) == 1;
+}
+
